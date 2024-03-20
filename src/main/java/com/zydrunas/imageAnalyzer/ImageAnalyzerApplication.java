b8c2dc44695aef1754a4f7076aa4999efa7c8fc3
Zydrunas <Znorvilas28@gmail.com>
2024-03-20 17:06:58 +0200

    initial commit

---
+++ src/main/java/com/zydrunas/imageAnalyzer/ImageAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.zydrunas.imageAnalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zydrunas/imageAnalyzer/ImageAnalizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zydrunas.imageAnalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageAnalizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
