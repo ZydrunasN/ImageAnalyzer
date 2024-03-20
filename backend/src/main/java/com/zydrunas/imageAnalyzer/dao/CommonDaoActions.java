bf36e52ede42234b91c569f75ccca3b95fa59202
Zydrunas <Znorvilas28@gmail.com>
2024-03-20 18:43:37 +0200

    [Back-End] Added CommonDaoActions interface

---
+++ backend/src/main/java/com/zydrunas/imageAnalyzer/dao/CommonDaoActions.java
@@ -0,0 +1,11 @@
+package com.zydrunas.imageAnalyzer.dao;
+
+import java.util.List;
+
+public interface CommonDaoActions<T> {
+    void save(T t);
+    void update(T t);
+    List<T> getAll();
+    T getByID(Long id);
+    void deleteByID(Long id);
+}
