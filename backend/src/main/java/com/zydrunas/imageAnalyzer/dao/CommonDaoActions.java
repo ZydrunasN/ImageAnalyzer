6ed217a06de0ae875ebf804ed581c99601a6cf09
Zydrunas <Znorvilas28@gmail.com>
2024-03-20 19:02:45 +0200

    [Back-End][Refactor] Changed to return optional

--- backend/src/main/java/com/zydrunas/imageAnalyzer/dao/CommonDaoActions.java
+++ backend/src/main/java/com/zydrunas/imageAnalyzer/dao/CommonDaoActions.java
@@ -1,11 +1,12 @@
 package com.zydrunas.imageAnalyzer.dao;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CommonDaoActions<T> {
     void save(T t);
     void update(T t);
-    List<T> getAll();
-    T getByID(Long id);
+    Optional<List<T>> getAll();
+    Optional<T> getByID(Long id);
     void deleteByID(Long id);
 }
